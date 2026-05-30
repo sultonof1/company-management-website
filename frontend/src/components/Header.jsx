6e667a0aa32609eaafc519fa118f1f49212f4ad4
sultonof1 <sultonofjorka@gmail.com>
2026-05-30 15:52:51 +0500

    Update Header.jsx

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react';
 import { LanguageContext } from '../App';
-import translations from '../translations.js';
+import translations from '../Translations.js';
 import '../styles/Header.css';
 
 const Header = () => {
