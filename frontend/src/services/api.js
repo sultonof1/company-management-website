bc57e470e1566b84f71982c3e1b80f68e1e0628c
sultonof1 <sultonofjorka@gmail.com>
2026-05-30 15:20:26 +0500

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api'
+  baseURL: 'https://sultonof-backend.onrender.com/api'
 });
 
 // Admin kirgan bo'lsa, xavfsizlik tokennini so'rovlarga avtomatik qo'shish
